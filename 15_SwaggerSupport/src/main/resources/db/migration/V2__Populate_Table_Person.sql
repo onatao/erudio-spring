7344fe571264ba703523b18cf89294629facd94c
Nathan Barros <nathanbarrosnt@gmail.com>
2023-05-25 15:49:15 -0300

    Swagger OPEN API

---
+++ 15_SwaggerSupport/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `adress`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'piriripompom', 'Nathan', 'Male', 'Barros'),
+	(2, 'pompompiriri', 'arthur', 'Male', 'barros'),
+	(3, 'pompompiriri', 'arthur', 'Male', 'barros'),
+	(4, 'mae', 'luciana', 'female', 'barros'),
+	(5, 'mae', 'clerverson', 'female', 'barros'),
+	(6, 'mae', 'clerverson', 'female', 'barros'),
+	(7, 'mae', '1232312', 'female', 'barros'),
+	(8, 'mae', '1232312', 'female', 'ba3232323rros');
